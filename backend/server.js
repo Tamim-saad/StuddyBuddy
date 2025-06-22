b306b21c8f1ff39c3e4d35f1ded8ea6a3f65fcc3
Sadiasithi <sadiasithi2001@gmail.com>
2025-06-22 17:26:12 +0600

    6th commit

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== "test") {
   connectDB(); // connectDB is async, you can await it or handle promise here
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server listening on port ${PORT}`);
   });
+  
 }
 
